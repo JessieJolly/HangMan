c8ed001fde2cca0b863d8318c4a488d47e4616f4
Jessica Jolly <jessicajolly4416@gmail.com>
2022-05-04 19:26:59 +0530

    created list of words and imported to game

--- game.py
+++ game.py
@@ -1,14 +1,20 @@
 import pygame
+import random as r
 
 pygame.init()
+
+#colors
+bgblue = "#82EEFD"
 
 screenDim = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
 pygame.display.set_caption("Let's play HangMan")
 gameover = False
+print(r.words[0])
 while(gameover == False):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameover = True
+    screenDim.fill(bgblue)
     pygame.display.update()
 pygame.quit()
 quit()
---
+++ random.py
@@ -0,0 +1,12 @@
+words = [
+    "dice",
+    "elephant",
+    "incurable",
+    "absent",
+    "leftovers",
+    "pocket",
+    "overrated",
+    "starbucks",
+    "abominable",
+    "disgusting"
+]
