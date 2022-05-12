6fa6f2a99685b18b5d73264eb1d0502dec120ce9
Jessica Jolly <jessicajolly4416@gmail.com>
2022-05-12 22:35:16 +0530

    resizable window

--- game.py
+++ game.py
@@ -9,24 +9,41 @@ black = "#242526"
 class HangMan:
 
     #printing characters
-    def print_char(c, self):
-        font_style = pygame.font.Font("EvilEmpire.ttf", 50) 
+    def print_char(xB, yB, c, self):
+        font_style = pygame.font.Font("EvilEmpire.ttf", 80) 
         mesg = font_style.render(c, True, black)
-        self.screenDim.blit(mesg, [100+self.offset, 180])
-        self.offset = self.offset + 100
+        self.screenDim.blit(mesg, [xB+100+self.offset, yB+180])
+        self.offset = self.offset + 50
 
     #game loop
     def game_loop(self):
-        self.screenDim.fill(bgblue)
+        xB = 0
+        yB = 0
+        self.screenDim.fill(bgblue) 
         for i in self.arr:
                 if(self.guessed_letters.count(i) == 0):
-                    HangMan.print_char('_', self)
+                    HangMan.print_char(xB, yB, '_', self)
                 else:
-                    HangMan.print_char(i, self)
+                    HangMan.print_char(xB, yB, i, self)
         while(self.gameover == False):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.gameover = True
+                if event.type == pygame.VIDEORESIZE: 
+                    self.screenDim.fill(bgblue)
+                    w, h = pygame.display.get_surface().get_size()
+                    self.offset = 0 
+                    if w!=800:
+                        xB = (w-800)/2
+                        yB = (h-600)/2
+                    else:
+                        xB = 0
+                        yB = 0  
+                    for i in self.arr:
+                        if(self.guessed_letters.count(i) == 0):
+                            HangMan.print_char(xB, yB, '_', self)
+                        else:
+                            HangMan.print_char(xB, yB, i, self)
             pygame.display.update()
 
     def __init__(self) -> None: 
